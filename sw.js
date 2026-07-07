0cc713caddc6d51b554cbfa73db6078775e25cf3
Aidan Salisbury <aidansalisbury213@gmail.com>
2026-07-07 15:33:30 -0600

    Remove orphan quality check from all pages, fix overlay display

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'po-manager-v83';
+var CACHE = 'po-manager-v84';
 var SHELL = ['/po-manager/', '/po-manager/index.html', '/po-manager/manifest.json', '/po-manager/icon-192.png', '/po-manager/icon-512.png', '/po-manager/apple-touch-icon.png', '/po-manager/panoramic-logo.png'];
 
 self.addEventListener('install', function(e) {
